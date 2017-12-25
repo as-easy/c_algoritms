e0f99eb62a365ffa3618f5b16dc9d31669a3c6f6
as-easy <varankinva@yandex.ru>
2017-12-26 01:58:12 +0300

    6 lesson - hash function

---
+++ 6_lesson/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_lesson
+{
+    /// <summary>
+    /// Автор: Варанкин Владимир
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+            {
+                textBox1.Text = " ";
+            }
+
+            HashFunction hash = new HashFunction();
+            textBox2.Text = hash.HashMetod(textBox1.Text).ToString();
+        }
+    }
+}
---
+++ 6_lesson/HashFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_lesson
+{
+    /// <summary>
+    /// Функция хэширования строки
+    /// </summary>
+    class HashFunction
+    {
+        public int HashMetod(string str)
+        {
+            int[] arr = new int[str.Length];
+            int sum = 0;
+
+            for ( int i=0; i<str.Length; i++)
+            {
+                arr[i] = (int) str[i];
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
